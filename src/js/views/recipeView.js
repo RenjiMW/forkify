b586da27baa5a840ee362a9244838450dd78eb48
Maciej <wielgosz.mac@gmail.com>
2023-12-29 20:29:34 +0100

    installation of Fracty

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -2,7 +2,8 @@ import View from './View.js';
 
 // import icons from '../img/icons.svg'; // Parcel 1
 import icons from 'url:../../img/icons.svg'; // Parcel 2
-import { Fraction } from 'fractional';
+// import { Fraction } from 'fractional';
+import fracty from 'fracty';
 // console.log(Fraction);
 
 class RecipeView extends View {
@@ -134,7 +135,7 @@ class RecipeView extends View {
   <use href="${icons}#icon-check"></use>
 </svg>
 <div class="recipe__quantity">${
-      ing.quantity ? new Fraction(ing.quantity).toString() : ''
+      ing.quantity ? fracty(ing.quantity).toString() : ''
     }</div>
 <div class="recipe__description">
   <span class="recipe__unit">${ing.unit}</span>
